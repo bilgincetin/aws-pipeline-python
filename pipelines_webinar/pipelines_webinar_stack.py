3e000756677e75ee051c1edde8a7112fe599faa5
Bilgihan Cetinkaya <bilgincetin@yahoo.com>
2021-04-24 17:16:30 +0200

    initial version

--- pipelines_webinar/pipelines_webinar_stack.py
+++ pipelines_webinar/pipelines_webinar_stack.py
@@ -1,15 +1,49 @@
-from aws_cdk import core as cdk
+from os import path
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+import aws_cdk.aws_lambda as lmb
+import aws_cdk.aws_apigateway as apigw
+import aws_cdk.aws_codedeploy as codedeploy
+import aws_cdk.aws_cloudwatch as cloudwatch
 
+class PipelinesWebinarStack(core.Stack):
 
-class PipelinesWebinarStack(cdk.Stack):
-
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        this_dir = path.dirname(__file__)
+
+        handler = lmb.Function(self, 'Handler',
+            runtime=lmb.Runtime.PYTHON_3_7,
+            handler='handler.handler',
+            code=lmb.Code.from_asset(path.join(this_dir, 'lambda')))
+
+        alias = lmb.Alias(self, 'HandlerAlias',
+            alias_name='Current',
+            version=handler.current_version)
+
+        gw = apigw.LambdaRestApi(self, 'Gateway',
+            description='Endpoint for a simple Lambda-powered web service',
+            handler=alias)
+
+        failure_alarm = cloudwatch.Alarm(self, 'FailureAlarm',
+            metric=cloudwatch.Metric(
+                metric_name='5XXError',
+                namespace='AWS/ApiGateway',
+                dimensions={
+                    'ApiName': 'Gateway',
+                },
+                statistic='Sum',
+                period=core.Duration.minutes(1)),
+            threshold=1,
+            evaluation_periods=1)
+
+        codedeploy.LambdaDeploymentGroup(self, 'DeploymentGroup',
+            alias=alias,
+            deployment_config=codedeploy.LambdaDeploymentConfig.CANARY_10_PERCENT_10_MINUTES,
+            alarms=[failure_alarm])
+
+        self.url_output = core.CfnOutput(self, 'Url',
+            value=gw.url)
+
